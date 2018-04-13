393727d7693046b81eb4f14db42b0b523a0514fc
xysss7 <xysasha@outlook.com>
2018-04-13 21:37:39 +0800

    History Activity & order activity

--- app/src/main/java/com/unnc/digitalmenu/intelligentdigitalmenu/MainActivity.java
+++ app/src/main/java/com/unnc/digitalmenu/intelligentdigitalmenu/MainActivity.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 
 
 
+
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -36,6 +38,45 @@ public class MainActivity extends AppCompatActivity
     private ImageView image8;
     private ImageView image9;
     private Button butexit;
+    private Button buttonplus1;
+    private Button buttonminus1;
+    private Button buttonplus2;
+    private Button buttonminus2;
+    private Button buttonplus3;
+    private Button buttonminus3;
+    private Button buttonplus4;
+    private Button buttonminus4;
+    private Button buttonplus5;
+    private Button buttonminus5;
+    private Button buttonplus6;
+    private Button buttonminus6;
+    private Button buttonplus7;
+    private Button buttonminus7;
+    private Button buttonplus8;
+    private Button buttonminus8;
+    private Button buttonplus9;
+    private Button buttonminus9;
+
+
+
+    private int num1;
+    private TextView textView1;
+    private int num2;
+    private TextView textView2;
+    private int num3;
+    private TextView textView3;
+    private int num4;
+    private TextView textView4;
+    private int num5;
+    private TextView textView5;
+    private int num6;
+    private TextView textView6;
+    private int num7;
+    private TextView textView7;
+    private int num8;
+    private TextView textView8;
+    private int num9;
+    private TextView textView9;
 
     private Button btnAdd,btnGetAll;
     private TextView student_Id;
@@ -164,6 +205,226 @@ public class MainActivity extends AppCompatActivity
                 dialog.show();
             }
         });
+
+        textView1 = (TextView)findViewById(R.id.etAmount1);
+        num1 = Integer.parseInt(textView1.getText().toString());
+        Button buttonplus1 = (Button)findViewById(R.id.buttonplus1);
+        buttonplus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = num1 + 1;
+                textView1.setText(Integer.toString(num1));
+            }
+        });
+
+        textView1 = (TextView)findViewById(R.id.etAmount1);
+        num1 = Integer.parseInt(textView1.getText().toString());
+        Button buttonminus1 = (Button)findViewById(R.id.buttonminus1);
+        buttonminus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num1 >= 1) {
+                    num1 = num1 - 1;
+                }
+                textView1.setText(Integer.toString(num1));
+            }
+        });
+
+        textView2 = (TextView)findViewById(R.id.etAmount2);
+        num1 = Integer.parseInt(textView1.getText().toString());
+        Button buttonplus2 = (Button)findViewById(R.id.buttonplus2);
+        buttonplus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num2 = num2 + 1;
+                textView2.setText(Integer.toString(num2));
+            }
+        });
+
+        textView2 = (TextView)findViewById(R.id.etAmount2);
+        num2 = Integer.parseInt(textView2.getText().toString());
+        Button buttonminus2 = (Button)findViewById(R.id.buttonminus2);
+        buttonminus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num2 >= 1) {
+                    num2 = num2 - 1;
+                }
+                textView2.setText(Integer.toString(num2));
+            }
+        });
+
+        textView3 = (TextView)findViewById(R.id.etAmount3);
+        num3 = Integer.parseInt(textView3.getText().toString());
+        Button buttonplus3 = (Button)findViewById(R.id.buttonplus3);
+        buttonplus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num3 = num3 + 1;
+                textView3.setText(Integer.toString(num3));
+            }
+        });
+
+        textView3 = (TextView)findViewById(R.id.etAmount3);
+        num3 = Integer.parseInt(textView3.getText().toString());
+        Button buttonminus3 = (Button)findViewById(R.id.buttonminus3);
+        buttonminus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num3 >= 1) {
+                    num3 = num3 - 1;
+                }
+                textView3.setText(Integer.toString(num3));
+            }
+        });
+
+        textView4 = (TextView)findViewById(R.id.etAmount4);
+        num4 = Integer.parseInt(textView4.getText().toString());
+        Button buttonplus4 = (Button)findViewById(R.id.buttonplus4);
+        buttonplus4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num4 = num4 + 1;
+                textView4.setText(Integer.toString(num4));
+            }
+        });
+
+        textView4 = (TextView)findViewById(R.id.etAmount4);
+        num4 = Integer.parseInt(textView4.getText().toString());
+        Button buttonminus4 = (Button)findViewById(R.id.buttonminus4);
+        buttonminus4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num4 >= 1) {
+                    num4 = num4 - 1;
+                }
+                textView4.setText(Integer.toString(num4));
+            }
+        });
+
+        textView5 = (TextView)findViewById(R.id.etAmount5);
+        num5 = Integer.parseInt(textView5.getText().toString());
+        Button buttonplus5 = (Button)findViewById(R.id.buttonplus5);
+        buttonplus5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num5 = num5 + 1;
+                textView5.setText(Integer.toString(num5));
+            }
+        });
+
+        textView5 = (TextView)findViewById(R.id.etAmount5);
+        num5 = Integer.parseInt(textView5.getText().toString());
+        Button buttonminus5 = (Button)findViewById(R.id.buttonminus5);
+        buttonminus5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num5 >= 1) {
+                    num5 = num5 - 1;
+                }
+                textView5.setText(Integer.toString(num5));
+            }
+        });
+
+        textView6 = (TextView)findViewById(R.id.etAmount6);
+        num6 = Integer.parseInt(textView6.getText().toString());
+        Button buttonplus6 = (Button)findViewById(R.id.buttonplus6);
+        buttonplus6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num6 = num6 + 1;
+                textView6.setText(Integer.toString(num6));
+            }
+        });
+
+        textView6 = (TextView)findViewById(R.id.etAmount6);
+        num6 = Integer.parseInt(textView6.getText().toString());
+        Button buttonminus6 = (Button)findViewById(R.id.buttonminus6);
+        buttonminus6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num6 >= 1) {
+                    num6 = num6 - 1;
+                }
+                textView6.setText(Integer.toString(num6));
+            }
+        });
+
+
+        textView7 = (TextView)findViewById(R.id.etAmount7);
+        num7 = Integer.parseInt(textView7.getText().toString());
+        Button buttonplus7 = (Button)findViewById(R.id.buttonplus7);
+        buttonplus7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num7= num7 + 1;
+                textView7.setText(Integer.toString(num7));
+            }
+        });
+
+        textView7 = (TextView)findViewById(R.id.etAmount7);
+        num7 = Integer.parseInt(textView7.getText().toString());
+        Button buttonminus7 = (Button)findViewById(R.id.buttonminus7);
+        buttonminus7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num7 >= 1) {
+                    num7 = num7 - 1;
+                }
+                textView7.setText(Integer.toString(num7));
+            }
+        });
+
+
+        textView8 = (TextView)findViewById(R.id.etAmount8);
+        num8 = Integer.parseInt(textView3.getText().toString());
+        Button buttonplus8 = (Button)findViewById(R.id.buttonplus8);
+        buttonplus8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num8 = num8 + 1;
+                textView8.setText(Integer.toString(num8));
+            }
+        });
+
+        textView8 = (TextView)findViewById(R.id.etAmount8);
+        num8 = Integer.parseInt(textView8.getText().toString());
+        Button buttonminus8 = (Button)findViewById(R.id.buttonminus8);
+        buttonminus8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num8 >= 1) {
+                    num8 = num8 - 1;
+                }
+                textView8.setText(Integer.toString(num8));
+            }
+        });
+
+        textView9 = (TextView)findViewById(R.id.etAmount9);
+        num9 = Integer.parseInt(textView9.getText().toString());
+        Button buttonplus9 = (Button)findViewById(R.id.buttonplus9);
+        buttonplus9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num9 = num9 + 1;
+                textView9.setText(Integer.toString(num9));
+            }
+        });
+
+        textView9 = (TextView)findViewById(R.id.etAmount9);
+        num9 = Integer.parseInt(textView9.getText().toString());
+        Button buttonminus9 = (Button)findViewById(R.id.buttonminus9);
+        buttonminus9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(num9 >= 1) {
+                    num9 = num9 - 1;
+                }
+                textView9.setText(Integer.toString(num9));
+            }
+        });
+
+
     }
 
     @Override
